542f8601fcf5c1b9f250fb15f430e5ebd97cb8ce
RodrigoMelicio <rodrigo.fortes-melicio@univ-tlse3.fr>
2025-03-04 15:59:38 +0100

    TP1 correction_1

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,11 +1,15 @@
 package personnages;
 
+import objets.Chaudron;
+
 public class Druide {
 	private String nom;
 	private int forcePotion;
+	private Chaudron chaudron;
 
 	public Druide(String nom) {
 		this.nom = nom;
+		this.chaudron = new Chaudron();
 	}
 
 	public void parler(String texte) {
@@ -14,11 +18,17 @@ public class Druide {
 
 	public void fabriquerPotion(int quantite, int forcePotion) {
 		this.forcePotion = forcePotion;
+		chaudron.remplirChaudron(quantite, forcePotion);
 		System.out.println("Le druide " + nom + " prépare une potion de force " + forcePotion);
 	}
 
 	public void booster(Gaulois gaulois) {
-		gaulois.boirePotion(forcePotion);
+		if (chaudron.resterPotion()) {
+			chaudron.prendreLouche();
+			gaulois.boirePotion(forcePotion);
+		} else {
+			System.out.println("Le Druide " + nom + " : \"Désolé, plus de potion !\"");
+		}
 	}
 
 	public String getNom() {
