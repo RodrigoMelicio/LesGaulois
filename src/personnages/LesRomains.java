d78d4314241b10838d459e871c0b274cc9856185
RodrigoMelicio <rodrigo.fortes-melicio@univ-tlse3.fr>
2025-09-25 11:57:45 +0200

    Finalisation des Romains

--- src/personnages/LesRomains.java
+++ src/personnages/LesRomains.java
@@ -1,5 +1,23 @@
 package personnages;
 
 public class LesRomains {
-
+	private String nom;
+	private int force;
+	public LesRomains(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+	
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	
+	
 }
